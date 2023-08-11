f59cc89dc786140eb0e2d971ce58adb9cc4339b2
auxerre2b <archyinuse@gmail.com>
2023-08-11 15:21:36 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='scrolling-image-container'>
+        <div className='scrolling-image'>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className=''>Short Earl</h1>
+        </main>
+        </div>
+    </div>
+  )
+}
